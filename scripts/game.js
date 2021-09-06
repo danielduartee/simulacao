afe52854f0f4e07db6431dae9a1fc2b857ac4d7d
danielduartee <danielduartee@outlook.com>
2021-09-06 20:41:05 -0300

    final

--- scripts/game.js
+++ scripts/game.js
@@ -7,28 +7,47 @@ function dead() {
 }
 function cajado() {
     var escolha = '';
-    escolha = prompt("Você escolheu o Cajado, mas emcontramos o priemiro desafio, não podemios fazer barulho. \n  Opação 1 vamos correndo \n vamos nadando");
+    escolha = prompt("Você escolheu o Cajado em uma boa hora, existe Zumbis se aproximando...  \n   1 Correr \n 2 Vamos lutar");
     while (escolha != '1' && escolha != '2') {
-        escolha = prompt("Você escolheu o Cajado, mas emcontramos o priemiro desafio, não podemios fazer barulho. \n  Opação 1 vamos correndo \n vamos nadando")
+        escolha = prompt("Você escolheu o Cajado em uma boa hora, existe Zumbis se aproximando...  \n   1 Correr \n 2 Vamos lutar");
     }
     if (escolha == '1') {
-        alert("Olha o zombie, precisamos passar por eles.")
+        alert("Corre, precisamos passar por eles.")
+        vitoria();
+        location.href = "file:///C:/Users/danie/Documents/GAME%20the%20GAME/html/game2.html";
     } else {
-        alert("fizemos baraulho demais, esse foi nosso erro!")
+        alert("Lutar não foi uma boa ideia, deveriamos ter corrido")
         location.href = "file:///C:/Users/danie/Documents/GAME%20the%20GAME/html/gameover.html";
     }
 }
 
 function arma() {
     var escolha = '';
-    escolha = prompt("Você escolheu o Cajado, mas emcontramos o priemiro desafio, não podemios fazer barulho. \n  Opação 1 vamos correndo \n vamos nadando");
+    escolha = prompt("Você escolheu a arma, mas encontramos o primiro desafio, não podemos fazer barulho tem zumbis vindo em nossa direção... \n  1 Vamos sair escondinho \n 2 Vamos Lutar");
     while (escolha != '1' && escolha != '2') {
-        escolha = prompt("Você escolheu o Cajado, mas emcontramos o priemiro desafio, não podemios fazer barulho. \n  Opação 1 vamos correndo \n vamos nadando")
+
+        escolha = prompt("Você escolheu a arma, mas encontramos o primiro desafio, não podemos fazer barulho tem zumbis vindo em nossa direção... \n  1 Vamos sair escondinho \n 2 Vamos Lutar");
     }
     if (escolha == '1') {
-        alert("Olha o zombie, precisamos passar por eles.")
+        alert("Calma Jovem, precisamos passar por eles em silencio");
+        vitoria();
     } else {
-      alert("fizemos baraulho demais, esse foi nosso erro!")
+        alert("Fizemos baraulho demais, esse foi nosso erro!");
         location.href = "file:///C:/Users/danie/Documents/GAME%20the%20GAME/html/gameover.html";
+    }
+}
+function vitoria() {
+    var winner = '';
+    winner = prompt("Isso! conseguimos passar, agora estamos proximo, existe duas opções, podemos ir pela rua ou pelo subsolo... \n 1 Subsolo \n 2 Rua");
+    while (winner != '1' && winner != '2') {
+        winner = prompt("Isso! conseguimos passar, agora estamos proximo, existe duas opções, podemos ir pela rua ou pelo subsolo... \n 1 Subsolo \n 2 Rua");
+    }
+    if (winner == '1') {
+        alert('Bom trabalho, passamos  e conseguimos completar a missão');
+        location.href = "file:///C:/Users/danie/Documents/GAME%20the%20GAME/html/game2.html"
+    } else {
+        alert("Dessa vez foi quase! A rua estava cheia de armardilhas e acabamos caindno nelas");
+        location.href = "file:///C:/Users/danie/Documents/GAME%20the%20GAME/html/gameover.html";
+
     }
 }
